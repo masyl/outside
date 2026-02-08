3484134d327558ce6cf8bd35627c530923e9f87e
Mathieu Sylvain <mathieu.s@vireoeducation.com>
2026-02-07 21:29:08 -0500

    chore: upgrade storybook to 10.2.7

--- packages/outside-storybook/.storybook/main.ts
+++ packages/outside-storybook/.storybook/main.ts
@@ -6,7 +6,7 @@ const rootDir = process.cwd();
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: ['@storybook/addon-essentials'],
+  addons: ['@storybook/addon-docs', '@storybook/addon-a11y'],
   framework: '@storybook/react-vite',
   docs: {
     autodocs: 'tag',
